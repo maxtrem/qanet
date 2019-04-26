fixing masking and parameters

bangliu_model.py
@@ -17,6 +17,9 @@ def mask_logits(target, mask):
     mask = mask.type(torch.float32)
     return target * mask + (1 - mask) * (-1e30)  # !!!!!!!!!!!!!!!  do we need * mask after target?
 
+def apply_logits(target, mask):
+    mask = mask.type(torch.float32)
+    return target * mask + (1 - mask) * (-1e30)  # !!!!!!!!!!!!!!!  do we need * mask after target?
 
 class Initialized_Conv1d(nn.Module):
     def __init__(self, in_channels, out_channels,
@@ -365,7 +368,7 @@ class QANet(nn.Module):
         self.emb = Embedding(wemb_dim, cemb_dim, d_model)
         self.num_head = num_head
         self.emb_enc = EncoderBlock(conv_num=4, d_model=d_model, num_head=num_head, k=7, dropout=0.1)
-        self.cq_att = ContextQueryAttention(d_model=d_model, dropout)
+        self.cq_att = ContextQueryAttention(d_model, dropout)
         self.cq_resizer = Initialized_Conv1d(d_model * 4, d_model)
         self.model_enc_blks = nn.ModuleList([EncoderBlock(conv_num=2, d_model=d_model, num_head=num_head, k=5, dropout=0.1) for _ in range(7)])
         self.out = Pointer(d_model)