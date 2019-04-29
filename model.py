372e2d71d96993cef7f752ff12731e5fc523aec2
Nyx <Nyxjemk@gmail.com>
2019-04-29 18:24:16 +0200

    adding test unit

--- model.py
+++ model.py
@@ -67,7 +67,7 @@ class QANet(nn.Module):
         super().__init__()
         
         self.c_limit, self.q_limit = c_limit, q_limit
-        
+
         self.input_embedding_layer = InputEmbedding(word_emb_matrix, char_emb_matrix, d_model=d_model, char_cnn_type=2)
 
         self.context_encoder       = EncoderBlock(d_model=d_model, seq_limit=c_limit, kernel_size=7, droprate=droprate, shared_norm=False)
@@ -112,12 +112,13 @@ class QANet(nn.Module):
 
         C = self.input_embedding_layer(cwids, ccids)
         Q = self.input_embedding_layer(qwids, qcids)
-        
+
         C = self.context_encoder(C, mask_C)
         Q = self.question_encoder(Q, mask_Q)
-        
+
         x = self.context_query_attn_layer(C, Q, mask_C, mask_Q)
         x = self.CQ_projection(x)
+
         enc_1 = self.forward_stacked_enc_blocks(x, mask_C)
         enc_2 = self.forward_stacked_enc_blocks(enc_1, mask_C)
         enc_3 = self.forward_stacked_enc_blocks(enc_2, mask_C)
@@ -131,4 +132,75 @@ class QANet(nn.Module):
     
 
         
-# ToDO: PosEncoder, AttnFlow, checking masking precedure (*?)
+
+if __name__ == "__main__":
+    torch.manual_seed(12)
+    test_EncoderBlock = False
+    test_QANet = True
+    test_PosEncoder = False
+
+    if test_EncoderBlock:
+        batch_size = 32
+        seq_length = 20
+        hidden_dim = 96
+        x = torch.rand(batch_size, seq_length, hidden_dim)
+        m = EncoderBlock(4, hidden_dim, 8, 7, seq_length)
+        y = m(x, mask=None)
+
+    if test_QANet:
+        # device and data sizes
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        wemb_vocab_size = 5000
+        wemb_dim = 300
+        cemb_vocab_size = 94
+        cemb_dim = 64
+        d_model = 32
+        batch_size = 32
+        q_max_len = 50
+        c_max_len = 400
+        char_dim = 16
+
+        # fake embedding
+        wv_tensor = torch.rand(wemb_vocab_size, wemb_dim)
+        cv_tensor = torch.rand(cemb_vocab_size, cemb_dim)
+
+        # fake input
+        question_lengths = torch.LongTensor(batch_size).random_(1, q_max_len)
+        question_wids = torch.zeros(batch_size, q_max_len).long()
+        question_cids = torch.zeros(batch_size, q_max_len, char_dim).long()
+        context_lengths = torch.LongTensor(batch_size).random_(1, c_max_len)
+        context_wids = torch.zeros(batch_size, c_max_len).long()
+        context_cids = torch.zeros(batch_size, c_max_len, char_dim).long()
+        for i in range(batch_size):
+            question_wids[i, 0:question_lengths[i]] = \
+                torch.LongTensor(1, question_lengths[i]).random_(
+                    1, wemb_vocab_size)
+            question_cids[i, 0:question_lengths[i], :] = \
+                torch.LongTensor(1, question_lengths[i], char_dim).random_(
+                    1, cemb_vocab_size)
+            context_wids[i, 0:context_lengths[i]] = \
+                torch.LongTensor(1, context_lengths[i]).random_(
+                    1, wemb_vocab_size)
+            context_cids[i, 0:context_lengths[i], :] = \
+                torch.LongTensor(1, context_lengths[i], char_dim).random_(
+                    1, cemb_vocab_size)
+
+        # test whole QANet
+        heads = 1
+        #qanet = QANet(wv_tensor, cv_tensor,
+        #              c_max_len, q_max_len, d_model)
+        #print(d_model, c_max_len, q_max_len, wv_tensor.shape, cv_tensor.shape)
+        with torch.no_grad():
+            qanet = QANet(d_model, c_max_len, q_max_len, wv_tensor, cv_tensor, droprate=0.1).to(device)
+            p1, p2 = qanet(context_wids, context_cids,
+                       question_wids, question_cids)
+        print(p1.shape)
+        print(p2.shape)
+
+    if test_PosEncoder:
+        m = PositionalEncoding(d_model=6, max_len=10, dropout=0)
+        input = torch.randn(3, 10, 6)
+        output = m(input)
+        print(output)
+        output2 = PosEncoder(input)
+        print(output2)
