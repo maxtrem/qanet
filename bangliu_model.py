7bdee41f1844291362f6983dd8f3fcf511a55097
Nyx <Nyxjemk@gmail.com>
2019-04-29 10:55:31 +0200

    adding posencoder

--- bangliu_model.py
+++ bangliu_model.py
@@ -212,7 +212,7 @@ class QANet(nn.Module):
         self.emb_enc = EncoderBlock(conv_num=4, d_model=d_model, num_head=num_head, k=7, dropout=0.1, pos_encoder=self.pos_encoder)
         self.cq_att = ContextQueryAttention(d_model, dropout)
         self.cq_resizer = Initialized_Conv1d(d_model * 4, d_model)
-        self.model_enc_blks = nn.ModuleList([EncoderBlock(conv_num=2, d_model=d_model, num_head=num_head, k=5, dropout=0.1) for _ in range(7)])
+        self.model_enc_blks = nn.ModuleList([EncoderBlock(conv_num=2, d_model=d_model, num_head=num_head, k=5, dropout=0.1, pos_encoder=self.pos_encoder) for _ in range(7)])
         self.out = Pointer(d_model)
         self.PAD = pad
         self.Lc = c_max_len
