791d18d9159c41e6417bb14bdbfcb2159a6c6a3b
Nyx <Nyxjemk@gmail.com>
2019-05-07 12:04:33 +0200

    adding 2.0 loader

--- squad_loader.py
+++ squad_loader.py
@@ -8,7 +8,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     
     
 class SQuADDataset(Dataset):
-    def __init__(self, npz_file, device=device, no_answer_id = 1000, no_answer_y=0):
+    def __init__(self, npz_file, device=device, no_answer_id=1000, no_answer_y=400):
         """
             npz_file:   numpy file containing the data
             num_steps:  (int) number of batches to generate - number of training steps
@@ -17,8 +17,8 @@ class SQuADDataset(Dataset):
         """
         # sets device for location of outputs
         self.device = device
-        self.na_id  = torch.tensor(no_answer_id)
-        self.na_y   = torch.tensor(no_answer_y)
+        self.na_id  = no_answer_id
+        self.na_y   = no_answer_y
         data = np.load(npz_file)
         # ids for context tokens and chars
         self.context_idxs = torch.from_numpy(data["context_idxs"]).long()
@@ -30,22 +30,23 @@ class SQuADDataset(Dataset):
         self.y1s = torch.from_numpy(data["y1s"]).long()
         self.y2s = torch.from_numpy(data["y2s"]).long()
         self.ids = torch.from_numpy(data["ids"]).long()
+        self.nas = (self.y2s == self.na_id)
+        self.y1s[self.nas] = self.na_y
+        self.y2s[self.nas] = self.na_y
+        
+    def num_nas(self):
+        return self.nas.sum().item()
 
     def __len__(self):
         return len(self.ids)
 
     def __getitem__(self, i):
-        no_answer = (self.y1s[i] == self.no_answer_id)
-        if bool(self.y1s[i] == self.no_answer_id) and bool(self.y2s[i] == self.no_answer_id):
-            a_start, a_end = self.na_y, self.na_y
-        else:
-            a_start, a_end = self.y1s[i], self.y2s[i]
         res = (self.context_idxs[i],        # context tokens
                self.context_char_idxs[i],   # context chars
                self.ques_idxs[i],           # question tokens
                self.ques_char_idxs[i],      # question chars
-               a_start, a_end,    # answer start, answer end
-               no_answer, self.ids[i]) # question id
+               self.y1s[i], self.y2s[i],    # answer start, answer end
+               self.nas[i], self.ids[i]) # question id
         
         # moving tensors to respective device
         res = tuple(map(lambda x: x.to(self.device), res))
